11710161e6eb9310bfc0e35a12655ad555c6d981
louis <114394306+ElDausss@users.noreply.github.com>
2024-11-20 15:13:10 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,19 +1,18 @@
-from flask import Flask, rendertemplatestring, rendertemplate, jsonify
-from flask import rendertemplate
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import render_template
 from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
 
-app = Flask(name)
+app = Flask(__name__)                                                                                                                  
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')
-
-@app.route('/contact/')
+    return render_template('hello.html') #comm2
+@app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
 
-if __name == "__main":
+if __name__ == "__main__":
   app.run(debug=True)
