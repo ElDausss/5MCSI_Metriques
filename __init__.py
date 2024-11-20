e4946759a5e6f306c0a1ccf7a9b343248c7a50a0
louis <114394306+ElDausss@users.noreply.github.com>
2024-11-20 15:22:49 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,6 +14,10 @@ def hello_world():
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
 
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
